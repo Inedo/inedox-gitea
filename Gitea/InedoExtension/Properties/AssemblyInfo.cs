7d009b00c3015a7d2922c667b974b62b213e9fd3
Greg Divis <gdivis@inedo.com>
2023-06-20 21:21:52 -0400

    fix assembly metadata

---
+++ Gitea/InedoExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Inedo.Extensibility;
+
+[assembly: AssemblyTitle("Gitea")]
+[assembly: AssemblyDescription("Provides integration with Gitea.")]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2023")]
+[assembly: AssemblyProduct("any")]
+
+// Not for ProGet
+[assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Otter)]
+
+[assembly: ScriptNamespace("Gitea")]
+
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
